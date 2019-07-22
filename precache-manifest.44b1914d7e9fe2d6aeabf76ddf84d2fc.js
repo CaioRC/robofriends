4ac09ef7981ea91dcd051e44abc85232852c8e4a
CaioRC <caiorcbr2@gmail.com>
2019-07-21 21:59:27 -0300

    Updates

---
+++ precache-manifest.44b1914d7e9fe2d6aeabf76ddf84d2fc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "143eb19aa518302daf32",
+    "url": "/robofriends/static/js/runtime~main.fb41d28d.js"
+  },
+  {
+    "revision": "de5bfaa2c6706fb34e2c",
+    "url": "/robofriends/static/js/main.4b8d4625.chunk.js"
+  },
+  {
+    "revision": "b1539a7d9a02e73cc4f0",
+    "url": "/robofriends/static/js/2.393f19e5.chunk.js"
+  },
+  {
+    "revision": "de5bfaa2c6706fb34e2c",
+    "url": "/robofriends/static/css/main.90a41735.chunk.css"
+  },
+  {
+    "revision": "b1539a7d9a02e73cc4f0",
+    "url": "/robofriends/static/css/2.952ae929.chunk.css"
+  },
+  {
+    "revision": "17ccbd5ee627e91febf54945c4c83c7d",
+    "url": "/robofriends/index.html"
+  }
+];
